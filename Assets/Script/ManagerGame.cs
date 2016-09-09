332eab4d7356fd70188e39c1fc098984cb127aa9
YasutakaAwano <eewano@gmail.com>
2016-09-09 18:57:53 +0900

    新規立ち上げ。

---
+++ Assets/Script/ManagerGame.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using UnityEngine;
+
+public class ManagerGame : MonoBehaviour {
+
+    private delegate void EventHandler(object sender, EventArgs e);
+
+    private enum Turnstate {
+        BATTLE_START,
+        TURN_PLAYER,
+        TURN_ENEMY,
+        RESULT
+    }
+
+    private Turnstate state;
+
+    private event EventHandler state_BATTLE_START;
+
+    private event EventHandler state_TURN_PLAYER;
+
+    private event EventHandler state_TURN_ENEMY;
+
+    private event EventHandler state_RESULT;
+
+    void Start() {
+    }
+
+    void Update() {
+        switch (state) {
+            case Turnstate.BATTLE_START:
+                break;
+            case Turnstate.TURN_PLAYER:
+                break;
+            case Turnstate.TURN_ENEMY:
+                break;
+            case Turnstate.RESULT:
+                break;
+        }
+    }
+
+    void Battle_Start() {
+        state = Turnstate.BATTLE_START;
+    }
+
+    void Turn_Player() {
+        state = Turnstate.TURN_PLAYER;
+    }
+
+    void Turn_Enemy() {
+        state = Turnstate.TURN_ENEMY;
+    }
+
+    void Result() {
+        state = Turnstate.RESULT;
+    }
+}
---
+++ Assets/Script/Status_Player01.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Status_Player01 {
+
+    private int hp_Player01;
+    private int hpMAX_Player01 = 50;
+
+    private int mp_Player01;
+    private int mpMAX_Player01 = 12;
+
+    private int atk_Player01 = 20;
+
+    private int def_Player01 = 15;
+
+    void Start() {
+        hp_Player01 = hpMAX_Player01;
+        mp_Player01 = mpMAX_Player01;
+    }
+
+    void Update() {
+    }
+}
